f546d74d5632c40dcaa1799ff512a08d159e6813
Emre ORHAN <emreorhan.ogr@gmail.com>
2025-03-20 14:42:20 +0300

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Emre on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
